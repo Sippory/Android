e2f22ca6b068f8bd08562cbc06d9afd9ed279044
lee0jae330 <xingxing2001@cau.ac.kr>
2025-11-22 07:11:02 +0900

    ✨ Feat: Add DrinkRepository for Firestore
    - Implement `DrinkRepository` to fetch drink data from Firestore.
    - Add `getAllDrinks` function to retrieve all drinks from the "drinks" collection.
    - Add `getDrinksByName` function to search for drinks with a case-insensitive prefix match, limiting results to 5.

---
+++ app/src/main/java/net/sippory/data/repository/DrinkRepository.kt
@@ -0,0 +1,37 @@
+package net.sippory.data.repository
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
+import net.sippory.data.dao.DrinkDao
+import net.sippory.data.entity.DrinkEntity
+
+class DrinkRepository(private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()) : DrinkDao {
+    private val drinksCollection = firestore.collection("drinks")
+
+    override suspend fun getAllDrinks(): List<DrinkEntity> {
+        return try {
+            val snapshot = drinksCollection.get().await()
+            snapshot.documents.mapNotNull { document ->
+                document.toObject(DrinkEntity::class.java)
+            }
+        } catch (e: Exception) {
+            println("Error fetching drinks: ${e.message}")
+            emptyList()
+        }
+    }
+
+    override suspend fun getDrinksByName(searchTerm: String): List<DrinkEntity> {
+        val editedSearchTerm = searchTerm[0].uppercase() + searchTerm.substring(1)
+        val endIndex = editedSearchTerm + "\uf8ff"
+
+        return try {
+            val snapshot =
+                drinksCollection.orderBy("name").startAt(editedSearchTerm).endAt(endIndex).limit(5).get().await()
+            snapshot.documents.mapNotNull { it.toObject<DrinkEntity>() }
+        } catch (e: Exception) {
+            println("Error fetching drinks by name: ${e.message}")
+            emptyList()
+        }
+    }
+}
