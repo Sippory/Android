6b8ce1f076f659bf2e37b65787dfe4b457f430b2
lee0jae330 <xingxing2001@cau.ac.kr>
2025-11-22 07:21:11 +0900

    ✨ Set SignInScreen as the start destination in NavGraph

--- app/src/main/java/net/sippory/navigation/NavGraph.kt
+++ app/src/main/java/net/sippory/navigation/NavGraph.kt
@@ -63,7 +63,7 @@ fun NavGraph(
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route,
+        startDestination = Screen.SignIn.route,
     ) {
         composable(Screen.SignIn.route) {
             SignInScreen(
