280a0465d729950328dafbe043a3404f21d1d186
YECHAN KIM <74256335+ii2001@users.noreply.github.com>
2025-11-28 20:34:33 +0900

    Add missing shadow import for HomeScreen

--- app/src/main/java/net/sippory/presentation/home/HomeScreen.kt
+++ app/src/main/java/net/sippory/presentation/home/HomeScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
