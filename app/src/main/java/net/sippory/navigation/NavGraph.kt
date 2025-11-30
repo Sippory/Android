1be421bc2f10a4c1cee623a2b50f8cf8d4993568
lee0jae330 <xingxing2001@cau.ac.kr>
2025-11-30 23:47:10 +0900

    ✨ Feat: Change start destination to SignInScreen

--- app/src/main/java/net/sippory/navigation/NavGraph.kt
+++ app/src/main/java/net/sippory/navigation/NavGraph.kt
@@ -80,7 +80,7 @@ fun NavGraph(
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route,
+        startDestination = Screen.SignIn.route,
     ) {
         composable(Screen.SignIn.route) {
             SignInScreen(
